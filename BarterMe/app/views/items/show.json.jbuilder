ffb56c5159020dfca1f79d144add0312767c8933
Bryan Turcotte <bdturc@brandeis.edu>
2015-02-11 15:35:47 -0500

    WS

---
+++ BarterMe/app/views/items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @item, :id, :name, :description, :image_url, :user_id, :product_key, :type_id, :location, :quantity, :post_date, :created_at, :updated_at
---
+++ BarterMe/db/migrate/20150211202851_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.string :image_url
+      t.integer :user_id
+      t.integer :product_key
+      t.integer :type_id
+      t.string :location
+      t.integer :quantity
+      t.integer :post_date
+
+      t.timestamps
+    end
+  end
+end
