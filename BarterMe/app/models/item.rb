40b88e9fdbe5abba2c551087594b8dc368f15a9c
wangyh0728 <wangyh@brandeis.edu>
2015-02-28 23:26:45 -0500

    Database_v1

--- BarterMe/app/models/item.rb
+++ BarterMe/app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
-searchkick word_start: [:name]
+	belongs_to :user, foreign_key: "user_id",inverse_of: :items
+	# validates :user, presence => true
+	validates_presence_of :user
+	searchkick word_start: [:name]
 end
--- BarterMe/app/models/user.rb
+++ BarterMe/app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-	has_many :items
+	has_many :items, inverse_of: :user
 end
