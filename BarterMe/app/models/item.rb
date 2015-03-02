b359bc7a7c3013f373a037bbb16b5f0561e3bcb3
Bryan Turcotte <bdturc@brandeis.edu>
2015-03-01 19:21:35 -0500

    item tests

--- BarterMe/app/models/item.rb
+++ BarterMe/app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
 searchkick word_start: [:name]
+validates :name, :description, :image_url, :post_date, :quantity, presence: true
+validates :quantity, numericality: {greater_than: 0}
 end
