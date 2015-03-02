8d1e4970b9ff653ee72adb4bcd8e39926aee5b2a
Bryan Turcotte <bdturc@brandeis.edu>
2015-03-01 19:26:48 -0500

    item tests

--- BarterMe/app/models/item.rb
+++ BarterMe/app/models/item.rb
@@ -1,9 +1,6 @@
 class Item < ActiveRecord::Base
-<<<<<<< HEAD
-=======
-	belongs_to :user, foreign_key: "user_id",inverse_of: :items
-	# validates :user, presence => true
-	validates_presence_of :user
-	searchkick word_start: [:name]
->>>>>>> 40b88e9fdbe5abba2c551087594b8dc368f15a9c
+  belongs_to :user, foreign_key: "user_id",inverse_of: :items
+  # validates :user, presence => true
+  validates_presence_of :user
+  searchkick word_start: [:name]
 end
