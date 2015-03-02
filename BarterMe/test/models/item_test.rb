a9de3912a5b06cde8b3c02033e3eb0679d22374e
Bryan Turcotte <bdturc@brandeis.edu>
2015-03-01 20:45:35 -0500

    item tests

--- BarterMe/test/models/item_test.rb
+++ BarterMe/test/models/item_test.rb
@@ -3,13 +3,47 @@ require 'pry'
 require 'pry-byebug'
 
 class ItemTest < ActiveSupport::TestCase
-  test "has a product in the database" do
-    assert Item.count, 1
+
+  test "can create a new product" do
+    product = Item.new
+    assert_instance_of Item, product
+  end
+  
+  test "has two products in the database" do
+    assert_equal Item.count, 2
   end
 
+  test "user cannot have duplicate items posted" do
+    product = Item.first
+    product2 = Item.last
+    assert product.invalid?
+    assert product2.invalid?
+    
+    product3 = Item.new(name: "My Item",
+                       description: "test item",
+                       image_url: "test.jpg",
+                       quantity: 5,
+                       post_date: 4444)
+
+    assert product3.valid? 
+    
+  end
+    
+  test "Can add and delete products from database" do
+    assert_equal Item.count, 2
+    product3 = Item.new(name: "My Item",
+                       description: "test item",
+                       image_url: "test.jpg",
+                       quantity: 5,
+                       post_date: 4444)
+    product3.save
+    assert_equal Item.count, 3
+    product3.destroy
+    assert_equal Item.count, 2
+  end
+  
   test "product attributes must not be empty" do
     product = Item.new
-    binding.pry
     assert product.invalid?
     assert product.errors[:name].any?
     assert product.errors[:description].any?
@@ -28,7 +62,7 @@ class ItemTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal ["must be greater than 0"],
     product.errors[:quantity]
-
+    
     product.quantity = -1
     assert product.invalid?
     assert_equal ["must be greater than 0"],
