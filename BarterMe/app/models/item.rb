1a0ad3328c06c90064af30c65620e75b47041853
Bryan Turcotte <bdturc@brandeis.edu>
2015-03-02 15:00:44 -0500

    reboot

--- BarterMe/app/models/item.rb
+++
@@ -1,6 +0,0 @@
-class Item < ActiveRecord::Base
-  belongs_to :user, foreign_key: "user_id",inverse_of: :items
-  # validates :user, presence => true
-  validates_presence_of :user
-  searchkick word_start: [:name]
-end
--- BarterMe/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-<<<<<<< HEAD
-	validates :name, presence: true, uniqueness :true 
-	has_secure_password
-	has_many :items
-=======
-	has_many :items, inverse_of: :user
->>>>>>> origin/master
-end
