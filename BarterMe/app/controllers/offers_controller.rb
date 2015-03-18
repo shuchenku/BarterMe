1c001fd692bd213e802955d011aa624a77adb0f9
SHU CHEN <chenshu8805@gmail.com>
2015-03-18 15:54:25 -0400

    cart half done

--- BarterMe/app/controllers/offers_controller.rb
+++ BarterMe/app/controllers/offers_controller.rb
@@ -69,6 +69,6 @@ class OffersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def offer_params
-      params.require(:offer).permit(:offer_id, :user1_id, :user2_id, :item1_id, :item2_id, :accepted)
+      params.require(:offer).permit(:user1_id, :user2_id, :item1_id, :item2_id)
     end
 end
--- BarterMe/app/models/offer.rb
+++ BarterMe/app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ActiveRecord::Base
+	 
 	 belongs_to :user1, class_name: "User", foreign_key: "user1_id"	 
 	 belongs_to :user2, class_name: "User", foreign_key: "user2_id" 
 	 validates_presence_of :user1_id
