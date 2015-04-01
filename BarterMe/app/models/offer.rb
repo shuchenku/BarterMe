d82f42c99b76b904e5054ef214e2a7d6560d10b7
SHU CHEN <chenshu8805@gmail.com>
2015-04-01 11:20:55 -0400

    offer v2.0

--- BarterMe/app/models/offer.rb
+++ BarterMe/app/models/offer.rb
@@ -4,7 +4,6 @@ class Offer < ActiveRecord::Base
 
 	belongs_to :user1, class_name: "User", foreign_key: "user1_id"	 
 	belongs_to :user2, class_name: "User", foreign_key: "user2_id" 
-  searchkick 
 
 	validates_presence_of :user1_id
 	validates_associated :user1
