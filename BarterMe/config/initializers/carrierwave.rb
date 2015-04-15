e56e9bd76c774ddf17f89633f6db888d647aab63
Bryan Turcotte <bdturc@brandeis.edu>
2015-04-15 13:36:56 -0400

    resize offer

--- BarterMe/config/initializers/carrierwave.rb
+++ BarterMe/config/initializers/carrierwave.rb
@@ -1,9 +1,13 @@
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_credentials = {
-    :provider => 'AWS', # required
-    :aws_access_key_id => ENV["AWS_ACCESS_KEY"], # required
-    :aws_secret_access_key => ENV["AWS_SECRET_KEY"] # required
-  }
-  config.fog_directory = ENV["AWS_BUCKET"] # required
+  if(Rails.env.production? || Rails.env.test?)
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider => 'AWS', # required
+      :aws_access_key_id => ENV["AWS_ACCESS_KEY"], # required
+      :aws_secret_access_key => ENV["AWS_SECRET_KEY"] # required
+    }
+    config.fog_directory = ENV["AWS_BUCKET"] # required
+  else
+    config.storage = :file
+  end
 end 
