a4bcfa6395da90a4ec69a9c7d9204f1182761e03
wangyh0728 <wangyh@brandeis.edu>
2015-04-29 17:00:12 -0400

    rating system

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421034247) do
+ActiveRecord::Schema.define(version: 20150429192421) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -75,6 +75,12 @@ ActiveRecord::Schema.define(version: 20150421034247) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "pending"
+  end
+
+  create_table "ratings", force: true do |t|
+    t.integer "offer_id"
+    t.integer "user_id"
+    t.float   "score"
   end
 
   create_table "users", force: true do |t|
