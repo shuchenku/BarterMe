11baabde54fbf02520dd9b080398a4850550d41c
Bryan Turcotte <bdturc@brandeis.edu>
2015-05-04 19:01:53 -0400

    minor fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,11 @@ class WelcomeController < ApplicationController
     @top_categories = Item.listing
     @total_items = Item.count
     recommended_barters = Offer.where(accepted: true)
-    @recommended_barter = barter_spotlight(recommended_barters.sample)
+    if recommended_barters != nil
+      @recommended_barter = barter_spotlight(recommended_barters.sample)
+    else
+      @recommended_barter = recommended_barters.sample
+    end
     if logged_in? && @current_user.looking_for.size >= 1
       best_matches = User.best_match(@current_user)
       @recommendations = best_matches.sample(3)
@@ -25,7 +29,7 @@ class WelcomeController < ApplicationController
     barter_data[0] = recommended_barter.user1.user_name
     barter_data[1] = recommended_barter.user2.user_name
     barter_data[2] = Item.find(recommended_barter.item1_id).name
-    barter_data[3] = Item.find(recommended_barter.item1_id).name
+    barter_data[3] = Item.find(recommended_barter.item2_id).name
     barter_data[4] = recommended_barter.user1.id
     barter_data[5] = recommended_barter.user2.id
     return barter_data
