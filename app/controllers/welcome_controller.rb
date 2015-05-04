358261d356c0e00c302eb78083b98da718000a47
SHU CHEN <chenshu8805@gmail.com>
2015-05-04 14:24:50 -0400

    carousel halfway done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,10 +6,11 @@ class WelcomeController < ApplicationController
     if logged_in? && @current_user.looking_for.size >= 1
       best_matches = User.best_match(@current_user)
       @recommendations = best_matches.sample(3)
-    end
-
-    if @recommendations.size<3
-      @recommendations.concat(Item.all.sample(3-@recommendations.size))
+      if @recommendations.size<3
+        @recommendations.concat(Item.all.sample(3-@recommendations.size))
+      end
+    else
+      @recommendations = Item.all.sample(3)
     end
   end
   
